9fc2ee10074bf4aa88ebd0177e6aed18f553d01c
Michael Petroff <petroff.mb@gmail.com>
2020-03-03 21:28:04 +0500

    add utils.go: добавлена функция проверки наличия файла

---
+++ cmd/repoindexer/utils.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"os"
+)
+
+// проверяет наличие файла на диске
+func fileExists(fp string) bool {
+	_, err := os.Stat(fp)
+	if  err == nil {
+		return true
+	}
+	return false
+}
